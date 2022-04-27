d8cb9ad7c27e3cc1602f149bbb6c1b23dc1168f5
ksherm45 <77851772+ksherm45@users.noreply.github.com>
2022-04-27 14:56:39 +0200

    completed first draft

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -10,37 +10,41 @@ function Projects() {
   
   <div className="thumbnail">
   <div className='title'> Kajio </div>
-  <figure>
   <a href='https://kajio.herokuapp.com/landingpage'>
-  <img src={Kajio} height={250} width={200} alt = 'Kajio Pic'/>
+  <img src={Kajio} height={250} width={'100%'} alt = 'Kajio Pic'/>
   </a>
-  </figure>
-  <p className='ProjectDesc'>A social media app made with HBS, CSS, HTML. Backend uses MongoDB and Express</p>
+  <p className='ProjectDesc'>A social media app made with HBS, Javascript CSS, HTML. Backend uses MongoDB and Express</p>
+  <div className='flex'>
   <a href='https://github.com/JohannesSattler/Kajio'>
-  <button className='button'>Code</button>
+  <button className='button' id='neon'>Code</button>
   </a>
-  <button className='button'>App</button>
+  <button className='button' id='neon'>App</button>
+  </div>
 </div>
 
 
   <div className="thumbnail">
-  <div className='title'>Collision Game! </div>
-    <figure>
-  <img src={DDD} height={250} width={200} alt = 'DDD Pic'/>
-  </figure>
+  <div className='title'>Door Dash Derby! </div>
+  <img src={DDD} height={250} width={'90%'} alt = 'DDD Pic'/>
+  <p className='ProjectDesc'>A basic X & Y axis collision game made with Javascript, HTML and CSS. Open with a live server in VS Code. </p>
+
+  <div className='flex'>
   <a href='https://github.com/ksherm45/Door-Dash-Derby'>
-    <button className='button'>Code</button>
+  <button className='button' id='neon'>Code</button>
   </a>
+  </div>
   </div>
 
   <div className="thumbnail">
   <div className='title'>My Portfolio </div>
-    <figure>
-  <img src={PortfolioPic} height={250} width={200} alt = 'DDD Pic'/>
-  </figure>
+  <img src={PortfolioPic} height={250} width={'90%'} alt = 'DDD Pic'/>
+  <p className='ProjectDesc'>Made with React, Javascript, CSS and HTML!</p>
+
+  <div className='flex'>
   <a href='https://github.com/ksherm45/my-portfolio'>
-    <button className='button'>Code</button>
+  <button className='button' id='neon'>Code</button>
   </a>
+  </div>
   </div>
 
 
